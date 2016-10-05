b5e1fe547fbbc6286c5cc82bae6f10c9269b4e7d
John Kayne <johnhoganuke2@gmail.com>
2016-10-05 14:29:35 -0400

    Update Climate.java

--- MechProgramming/src/main/java/climate/Climate.java
+++ MechProgramming/src/main/java/climate/Climate.java
@@ -1,1 +1,11 @@
-public final class Climate {}
+package climate;
+
+public final class Climate {
+  public static float temperature;
+  public static float humidity;
+  
+  public void update() {
+    this.temperature = Instruments.thermometers.check();
+    this.humidity = Instruments.hygrometers.check();
+  }
+}
